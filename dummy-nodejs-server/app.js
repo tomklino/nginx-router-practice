c60d9566fb8f07442edda0fdb9428f891d5ba74f
Tom Klinovsky <tom@kiwinet.co.il>
2018-09-17 18:39:04 +0300

    extended the dummy nodejs server to serve files if www_dir env var is defined

--- dummy-nodejs-server/app.js
+++ dummy-nodejs-server/app.js
@@ -1,15 +1,38 @@
 // content of index.js
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const port = process.env['DUMMY_PORT']
+const www_dir = process.env['WWW_DIR']
 
-const requestHandler = (request, response) => {
+const simpleRequestHandler = (request, response) => {
   console.log(request.url)
   response.end(`
-    Hello! My name is ${process.env['DUMMY_NAME']}\n
-    and I got a request to ${request.url}\n`)
+    Hello! My name is ${process.env['DUMMY_NAME']}
+    and I got a request to ${request.url}`)
 }
 
-const server = http.createServer(requestHandler)
+const fileServer = (request, response) => {
+  console.log(request.url)
+  fs.readFile(
+    path.join(www_dir, request.url),
+    {encoding: "utf8"},
+    (e, data) => {
+      if(e) {
+        if(e.code === "ENOENT") {
+          response.statusCode = 404;
+          response.end("404 not found");
+          return;
+        }
+        response.statusCode = 500;
+        response.end("Internal server error")
+        return;
+      }
+      response.end(data)
+    })
+}
+
+const server = http.createServer(www_dir ? fileServer : simpleRequestHandler)
 
 server.listen(port, (err) => {
   if (err) {
