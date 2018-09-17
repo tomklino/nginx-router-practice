8e6e8daf527dd0591d446ecd5a254eeb07698ff5
Tom Klinovsky <tom@kiwinet.co.il>
2018-09-17 17:51:52 +0300

    initial commit

---
+++ dummy-nodejs-server/app.js
@@ -0,0 +1,20 @@
+// content of index.js
+const http = require('http')
+const port = process.env['DUMMY_PORT']
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end(`
+    Hello! My name is ${process.env['DUMMY_NAME']}\n
+    and I got a request to ${request.url}\n`)
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
